d47775f611cafe80e2fb60d8c42495ac13088549
Rarmstrong45 <Ryan_armstrong07@yahoo.com>
2021-08-25 20:27:07 -0500

    updating models - updated Category.js and Index.j

--- models/index.js
+++ models/index.js
@@ -6,11 +6,31 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'caterogy_id'
+})
+
 // Categories have many Products
+
+Category.hasMany(product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'product_id'
+})
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
